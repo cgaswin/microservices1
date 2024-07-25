12154c5bcaece0f5e970fb7f891a764acad478a0
cgaswin <cgaswin333@gmail.com>
2024-07-25 17:42:33 +0530

    Added product service to test feign client

--- openfeignclient/src/main/java/com/iiht/configclient/resources/FeignClient.java
+++ openfeignclient/src/main/java/com/iiht/configclient/resources/FeignClient.java
@@ -12,24 +12,27 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/client")
-public class FeignClient {
+public class FeignClient implements FeignInterface {
     @Autowired
     private FeignInterface feignClient;
 
+    @Override
     @GetMapping("/allProducts")
-
-    public List<Product> getAll() {
+    public List<Product> getProducts() {
         return feignClient.getProducts();
     }
-
+    
     //http://localhost:7082/client/products-by-id/2
+    @Override
     @GetMapping("/products-by-id/{id}")
-    public Product getProductById(@PathVariable ("id") int id) {
+    public Product getProductsById(@PathVariable ("id") int id) {
         System.out.println(id);
         return feignClient.getProductsById(id);
     }
 
+
     // http://localhost:7082/client/products-by-category/Mobile
+    @Override
     @GetMapping("/products-by-category/{category}")
     public List<Product> getProductsByCategory(@PathVariable("category") String category){
         return feignClient.getProductsByCategory(category);
